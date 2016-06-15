ead1d3591db23a0f4d48e85ecd527c56615017af
wcongying <wcongying@126.com>
2016-06-15 14:42:39 -0700

    Update article.php

--- article.php
+++ article.php
@@ -6,15 +6,14 @@
 
 	if($id == ''){
 	$info = <<<INFO
-	<script>window.alert('获取的id不正确'); history.back();</script>
+	<script>window.alert('Wrong ID'); history.back();</script>
 INFO;
 	echo $info;
 	}
 
 	$sql = "SELECT * FROM `tn_article` WHERE id={$id}";
 
-	//对dao工具类，添加一个方法，只返回一条记录.
+
 	$article = $dao->fetchRow($sql);
 	
-	//引入模板
 	require TMP_PATH . 'article.html';
